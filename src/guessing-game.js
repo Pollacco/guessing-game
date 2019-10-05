c513864aaae7a1d884e776da0b269239be76c30b
Pollacco <andrey.sviontek@gmail.com>
2019-10-05 14:26:10 +0500

    solution: something goes wrong

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,21 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.roll = Math.floor(Math.random() * (max - min)) + min;
+        return this.roll;
     }
 
     lower() {
-
+        this.max = this.roll;
     }
 
     greater() {
-
+        this.min = this.roll;
     }
 }
 
